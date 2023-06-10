3275fe18be48afba349004267a8ae1836120a7ae
wizardforcel <562826179@qq.com>
2023-06-10 23:28:54 +0800

    2023-06-10 23:28:54

--- BookerUtils/__init__.py
+++ BookerUtils/__init__.py
@@ -5,9 +5,9 @@ __email__ = "wizard.z@qq.com"
 __license__ = "SATA"
 __version__ = "2023.6.10.0"
 
-from except_ import *
-from ext import *
-from file import *
-from net import *
-from obj import *
-from text import *
+from .except_ import *
+from .ext import *
+from .file import *
+from .net import *
+from .obj import *
+from .text import *
